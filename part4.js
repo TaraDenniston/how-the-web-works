6ca2cf755dfef3d55ff693d9facc3a00bd23b456
Tara Denniston <taradenniston@gmail.com>
2022-08-13 09:43:50 -0400

    initial commit

---
+++ part4.js
@@ -0,0 +1,8 @@
+// Part Four: Explore the URL API
+
+// 1. At times, it’s useful for your JavaScript to look at the URL of the browser 
+//    window and change how the script works depending on parts of that (particularly 
+//    the query string).
+// 2. Read about the URL API at https://developer.mozilla.org/en-US/docs/Web/API/URL
+// 3. Try some of the code examples in the Chrome Console so that you can get 
+//    comfortable with the basic methods and properties for instances of the URL class.
